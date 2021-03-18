c4decde371579a8bfb2b81f09f42faae29b672c8
Kayla Luebbert <luebbertkayla@gmail.com>
2021-03-18 01:40:47 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -20,7 +20,7 @@ class Patient
     Appointment.all.select {|appointment| appointment.patient == self}
   end
   
-  def artists 
-    songs.collect {|song| song.artist}
+  def doctors 
+    appointments.collect {|appointment| appointment.doctor}
   end
 end
