417b454431a6adb722e977c056b9e3feccf34012
Kayla Luebbert <luebbertkayla@gmail.com>
2021-03-18 01:36:34 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,26 +1,16 @@
 class Appointment 
-  attr_accessor :name, :doctor, :patient 
+  attr_accessor :date, :doctor, :patient 
   
   @@all = []
   
-  def initialize(name)
-    @name = name 
+  def initialize(date, patient, doctor)
+    @date = date 
+    @patient = patient 
+    @doctor = doctor
     @@all << self
   end 
   
   def self.all 
     @@all
   end 
-  
-  def new_song(name, artist)
-    Song.new(name, artist, self)
-  end
-  
-  def songs
-    Song.all.select {|song| song.genre == self}
-  end
-  
-  def artists 
-    songs.collect {|song| song.artist}
-  end
 end
