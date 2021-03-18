e29233f05e14b2078be8a201f7a9bf0cb8500926
Kayla Luebbert <luebbertkayla@gmail.com>
2021-03-18 01:47:05 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,16 +1,15 @@
-class Appointment 
-  attr_accessor :date, :doctor, :patient 
-  
+class Appointment
+
+  attr_accessor :date, :patient, :doctor
   @@all = []
-  
-  def initialize(date, patient, doctor)
-    @date = date 
-    @patient = patient 
+
+  def initialize(patient, date, doctor)
+    @patient = patient
+    @date = date
     @doctor = doctor
     @@all << self
-  end 
-  
-  def self.all 
+  end
+
+  def self.all
     @@all
-  end 
-end
+  end
